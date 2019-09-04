68f3e0d46bde4061ddc3bd352bfbfe21bd6d3059
agonzalez-2018036@kinal.edu.gt <agonzalez-2018036@kinal.edu.gt>
2019-09-03 20:00:27 -0600

    actualizacion

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -14,8 +14,10 @@ public class HolaMundo {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {     
         // TODO code application logic here
+        System.out.println("hola  mundo xd");
+        System.out.println("actualizacion");
     }
     
 }
